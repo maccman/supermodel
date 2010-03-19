e078c36bdf200d266cbadf86ed484d9ee3a2d7dc
Alex MacCaw <maccman@gmail.com>
2010-03-19 00:55:13 +0000

    Add basic associations

--- lib/supermodel/association.rb
+++ lib/supermodel/association.rb
@@ -3,21 +3,39 @@ require "active_support/core_ext/string/inflections.rb"
 module SuperModel
   module Association
     module ClassMethods
-      def belongs_to(model)
-        model = model.to_s
-        model_name = model.classify
+      def belongs_to(to_model, options = {})
+        to_model    = to_model.to_s
+        class_name  = options[:class_name]  || to_model.classify
+        foreign_key = options[:foreign_key] || "#{to_model}_id"
+        primary_key = options[:primary_key] || "id"
+        
         class_eval(<<-EOS, __FILE__, __LINE__ + 1)
-          def #{model}                                          # def user
-            #{model_name}.find(#{model}_id)                     #   User.find(user_id)
-          end                                                   # end
-                                                                # 
-          def #{model}?                                         # def user?
-            #{model}_id && #{model_name}.exists?(#{model}_id)   #   user_id && User.exists?(user_id)
-          end                                                   # end
-                                                                # 
-          def #{model}=(model)                                  # def user=(model)
-            self.#{model}_id = (model && model.id)              #   self.user_id = (model && model.id)
-          end                                                   # end
+          def #{to_model}                                             # def user
+            #{foreign_key} && #{class_name}.find(#{foreign_key})      #   user_id && User.find(user_id)
+          end                                                         # end
+                                                                      # 
+          def #{to_model}?                                            # def user?
+            #{foreign_key} && #{class_name}.exists?(#{foreign_key})   #   user_id && User.exists?(user_id)
+          end                                                         # end
+                                                                      # 
+          def #{to_model}=(object)                                    # def user=(model)
+            self.#{foreign_key} = (object && object.#{primary_key})   #   self.user_id = (model && model.id)
+          end                                                         # end
+        EOS
+      end
+      
+      def has_many(to_model, options = {})
+        to_model    = to_model.to_s
+        class_name  = options[:class_name]  || to_model.classify
+        foreign_key = options[:foreign_key] || "#{model_name.singular}_id"
+        primary_key = options[:primary_key] || "id"
+        class_eval(<<-EOS, __FILE__, __LINE__ + 1)
+          def #{to_model}                                             # def user
+            #{class_name}.find_all_by_attribute(                      #   User.find_all_by_attribute(
+              :#{foreign_key},                                        #    :task_id,
+              #{primary_key}                                          #    id
+            )                                                         #   )
+          end                                                         # end
         EOS
       end
     end
